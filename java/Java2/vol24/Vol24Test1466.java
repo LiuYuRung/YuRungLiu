a113e828efffca329f93bc66ecf07595a7629dfb
LIUYURUNG <yvonne19970903.gmail.com>
2025-01-15 08:16:33 +0900

    update

---
+++ java/Java2/vol24/Vol24Test1466.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+class Vol24Test1466{
+    public static void main(String[] args){
+        HashSet<String> set = new HashSet<String>();
+
+        set.add("Toyko");
+        set.add("Paris");
+        set.add("London");
+
+        Iterator<String> iterator = set.iterator();
+        while(iterator.hasNext()){
+            String element = iterator.next();
+            System.out.println(element);
+        }        
+    }
+}
